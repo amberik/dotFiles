f4fe5b5c8465a3f430ce14f333bdd5c93c8469ec
kovals1 <sergey.kovalev@emc.com>
2017-04-24 06:12:08 -0400

    Fix update of config files

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+rm ~/.zshrc
+rm ~/.tmux.conf
+rm ~/.vimrc
+rm ~/.gitconfig
+
 ln ./zsh/zshrc      ~/.zshrc
 ln ./tmux/tmux.conf ~/.tmux.conf
 ln ./vim/vimrc      ~/.vimrc
 ln ./git/gitconfig  ~/.gitconfig
 
-curl -L git.io/antigen > ~/antigen.zsh
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
+if [ !  ~/antigen.zsh ] 
+then
+    curl -L git.io/antigen > ~/antigen.zsh
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
