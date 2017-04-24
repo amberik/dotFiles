30c334fecd169cbe4960f20aee9c83c8388ca111
kovals1 <sergey.kovalev@emc.com>
2017-04-24 06:46:26 -0400

    1. Disable spell check in vim. 2. Fix ./setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-rm ~/.zshrc
-rm ~/.tmux.conf
-rm ~/.vimrc
-rm ~/.gitconfig
 
-ln ./zsh/zshrc      ~/.zshrc
-ln ./tmux/tmux.conf ~/.tmux.conf
-ln ./vim/vimrc      ~/.vimrc
-ln ./git/gitconfig  ~/.gitconfig
+function updateConfigFile () {
+    sourceConfigFile=$1 
+    configFile=$2
+    echo "Remove the file: $configFile"
+    rm "$configFile"
+    echo "Create link $sourceConfigFile => $configFile"
+    ln "$sourceConfigFile" "$configFile"
+}
+
+updateConfigFile ./zsh/zshrc ~/.zshrc
+updateConfigFile ./tmux/tmux.conf ~/.tmux.conf
+updateConfigFile ./vim/vimrc  ~/.vimrc
+updateConfigFile ./git/gitconfig ~/.gitconfig
 
 if [ !  ~/antigen.zsh ] 
 then
